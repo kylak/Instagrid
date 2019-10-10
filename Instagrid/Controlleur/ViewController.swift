b7c3920b2d9a3ca257eeba7160803c18c26c3143
kylak <gustavberloty2@gmail.com>
2019-10-10 12:33:59 +0200

    Changement des grilles fonctionnelles

--- Instagrid/Controlleur/ViewController.swift
+++ Instagrid/Controlleur/ViewController.swift
@@ -10,12 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var TopRightButton: UIButton!
+    @IBOutlet weak var BottomRightButton: UIButton!
+    
+    @IBOutlet weak var Layout1Button: UIButton!
+    @IBOutlet weak var Layout2Button: UIButton!
+    @IBOutlet weak var Layout3Button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
+        Layout1ButtonTouched(" (executed for a default view) ")
     }
-
-
+    
+    @IBAction func Layout1ButtonTouched(_ sender: Any) {
+        if (Layout1Button.currentImage == nil) {
+            removeButtonsImages()
+            BottomRightButton.isHidden = false;
+            Layout1Button.setImage(UIImage(named: "Selected"), for: UIControl.State.normal)
+            TopRightButton.isHidden = true;
+        }
+    }
+    
+    @IBAction func Layout2ButtonTouched(_ sender: Any) {
+        if (Layout2Button.currentImage == nil) {
+            removeButtonsImages()
+            TopRightButton.isHidden = false;
+            Layout2Button.setImage(UIImage(named: "Selected"), for: UIControl.State.normal)
+            BottomRightButton.isHidden = true;
+        }
+    }
+    
+    @IBAction func Layout3ButtonTouched(_ sender: Any) {
+        if (Layout3Button.currentImage == nil) {
+            removeButtonsImages()
+            TopRightButton.isHidden = false;
+            BottomRightButton.isHidden = false;
+            Layout3Button.setImage(UIImage(named: "Selected"), for: UIControl.State.normal)
+        }
+    }
+    
+    func removeButtonsImages() {
+        Layout1Button.setImage(nil, for: UIControl.State.normal)
+        Layout2Button.setImage(nil, for: UIControl.State.normal)
+        Layout3Button.setImage(nil, for: UIControl.State.normal)
+    }
 }
 
